5e59d7eb49cb3943cac0358fe2fbfadd394a6bc0
blessedjoe2000 <blessedjoe2000@yahoo.com>
2024-09-05 10:36:50 -0500

    footer changed

--- app/component/Footer/Footer.jsx
+++ app/component/Footer/Footer.jsx
@@ -3,23 +3,8 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div className="bg-black-green text-white p-10 sm:p-20 sm:flex justify-between items-center gap-10">
-      <Link href="/" className=" font-roboto text-xl font-bold">
-        Anikus LLC
-      </Link>
-
-      <div className="sm:flex justify-between items-center lg:gap-80 md:gap-40">
-        <div className="flex flex-col justify-around items-center gap-2 text-lg">
-          <Link href="/">Home</Link>
-          <Link href="/about">About Us</Link>
-          <Link href="/">Contact Us</Link>
-        </div>
-        <div className="flex flex-col justify-around items-center gap-2 text-lg">
-          <Link href="/">Home</Link>
-          <Link href="/about">About Us</Link>
-          <Link href="/">Contact Us</Link>
-        </div>
-      </div>
+    <div className="bg-black-green text-white p-5 sm:p-10 text-center">
+      <p>Anikus LLC. All Rights Reserved. ©{new Date().getFullYear()}</p>
     </div>
   );
 }
